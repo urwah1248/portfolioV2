5c7452a8095e551f8ccf765fa938a12618a954f7
M Urwah Shahid <urwahpatel@gmail.com>
2022-11-02 00:11:04 +0500

    fixed footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,21 +15,26 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="hidden left-fixedbar md:block fixed w-6 h-[400px] bottom-0 right-5">
+      <div className="hidden right-fixedbar md:block fixed w-6 h-[400px] bottom-0 right-5">
         <div className="flex flex-col gap-4">
             <a className='iconhover mb-[164px] rotate-90 text-gray-400 font-mono' href='mailto:urwahpatel@gmail.com'>urwahpatel@gmail.com</a>
         </div>
         <div className='relative w-1 h-80 bg-gray-400 mx-auto'></div>
-        
       </div>
 
-      <div className="md:hidden links flex flex-row w-1/2 mx-auto justify-evenly h-8 my-5">
+      <div className='md:hidden links flex flex-row w-2/3 mx-auto justify-evenly h-8 my-5'>
+          <a className='w-8' href="https://github.com/urwah1248" target='_blank' rel="noreferrer"><IconGitHub/></a>
+          <a className='w-8' href="https://www.linkedin.com/in/m-urwah-shahid-053a38229/" target='_blank' rel="noreferrer"><IconLinkedin/></a>
+          <a className='w-8' href="https://twitter.com/shahid_urwah" target='_blank' rel="noreferrer"><IconTwitter/></a>
+      </div>
+      
+      {/* <div className="md:hidden links flex flex-row  w-1/2 mx-auto justify-evenly h-8 my-5">
           <a href="https://github.com/urwah1248" target='_blank' rel="noreferrer"><IconGitHub/></a>
           <a href="https://www.linkedin.com/in/m-urwah-shahid-053a38229/" target='_blank' rel="noreferrer"><IconLinkedin/></a>
           <a href="https://twitter.com/shahid_urwah" target='_blank' rel="noreferrer"><IconTwitter/></a>
-      </div>
+      </div> */}
 
-      <p className='font-mono text-center text-gray-500'>All rights reserved. Created by Muhammad Urwah</p>
+      <p className='font-mono text-center text-gray-500'>All rights reserved. <br className='md:hidden'/> Created by Muhammad Urwah</p>
     </div>
   )
 }
