362ac488c9dd6bf4a958c11a507b9488cffee8d8
M Urwah Shahid <107793187+urwah1248@users.noreply.github.com>
2023-05-14 15:27:52 +0500

    Fixed Linkedin URL

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
 
       <div className='md:hidden links flex flex-row w-2/3 mx-auto justify-evenly h-8 my-5'>
           <a className='w-8' href="https://github.com/urwah1248" target='_blank' rel="noreferrer"><IconGitHub/></a>
-          <a className='w-8' href="https://www.linkedin.com/in/m-urwah-shahid-053a38229/" target='_blank' rel="noreferrer"><IconLinkedin/></a>
+          <a className='w-8' href="https://www.linkedin.com/in/muhammad-urwah/" target='_blank' rel="noreferrer"><IconLinkedin/></a>
           <a className='w-8' href="https://twitter.com/shahid_urwah" target='_blank' rel="noreferrer"><IconTwitter/></a>
       </div>
       
