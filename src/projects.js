d217f7e8a469ab43b23828a457e194c8ef7acaee
Muhammad Urwah <urwahpatel@gmail.com>
2023-06-19 00:57:50 +0500

    Updated Projects

--- src/projects.js
+++ src/projects.js
@@ -8,18 +8,18 @@ import p6 from './media/project6.png'
 
 const projects = [
     {
+        title: "Explain it GPT",
+        detail: "AI application that uses ChatGPT API to explain terms.",
+        img: p1,
+        repo: "https://github.com/urwah1248/Explain-it-GPT",
+        prev: "https://explain-it-gpt.vercel.app"
+    },
+    {
         title: "Fashio Store",
         detail: "Full stack Ecommerce Store Built using Next JS, Node JS and MongoDB. Work in Progress....",
         img: p6,
         repo: "https://github.com/urwah1248/next-ecommerce-app-fashio-store",
         prev: "https://fashiostore.vercel.app"
-    },
-    {
-        title: "Little Lemon's Restaurant",
-        detail: "Website for the Little Lemon's Restaurant built using plain HTML, CSS and JS",
-        img: p4,
-        repo: "https://github.com/urwah1248/Little-Lemon-Restaurant-Website",
-        prev: "https://urwah1248.github.io/Little-Lemon-Restaurant-Website/"
     },
     {
         title: "Apple Clone",
@@ -29,11 +29,11 @@ const projects = [
         prev: "https://urwah1248.github.io/Apple-Site-Clone/"
     },
     {
-        title: "Expense Tracker",
-        detail: "An app to track your expenses records. Built using react.",
-        img: p1,
-        repo: "https://github.com/urwah1248/expense-tracker-react",
-        prev: "https://expense-tracker-urwah1248.netlify.app/"
+        title: "Little Lemon's Restaurant",
+        detail: "Website for the Little Lemon's Restaurant built using plain HTML, CSS and JS",
+        img: p4,
+        repo: "https://github.com/urwah1248/Little-Lemon-Restaurant-Website",
+        prev: "https://urwah1248.github.io/Little-Lemon-Restaurant-Website/"
     },
     {
         title: "Know the Countries",
